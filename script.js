426fb74213ee8fdfe14e211f657b9000f2444a67
celine333 <56337413+celine333@users.noreply.github.com>
2021-06-09 17:07:17 +0200

    changes

--- script.js
+++ script.js
@@ -2,8 +2,18 @@
 var Lightning;
 (function (Lightning) {
     window.addEventListener("load", handleLoad);
+    let selectedcolor = "#ff0000";
     function handleLoad(_event) {
         console.log("Anwendung startet");
+        // Farbe der Kategorie
+        let music = document.querySelector("#musicid");
+        music.addEventListener("click", setColor);
+        let film = document.querySelector("#filmid");
+        film.addEventListener("click", setColor);
+        let sport = document.querySelector("#sportid");
+        sport.addEventListener("click", setColor);
+        let fitness = document.querySelector("#fitnessid");
+        fitness.addEventListener("click", setColor);
         // Blitz Bild
         let lightning = document.querySelector("#lightningid");
         lightning.addEventListener("click", startApp);
@@ -12,6 +22,9 @@ var Lightning;
         house.addEventListener("click", NavigateToMenu);
         // let backarrow: HTMLElement = <HTMLElement>document.querySelector("#arrowid");
         // backarrow.addEventListener("click", NavigateBack);
+        // Status
+        let statusfield = document.querySelector("#statusid");
+        statusfield.addEventListener("click", startStatus);
         // Chat
         let chatfield = document.querySelector("#chatfieldid");
         chatfield.addEventListener("click", startChat);
@@ -38,11 +51,18 @@ var Lightning;
         console.log("navigieren erfolgreich");
         let menu = document.querySelector("#menuid");
         let chat = document.querySelector("#chatid");
+        let status = document.querySelector("#categoryid");
         if (menu.style.display == "none") {
             menu.style.display = "block";
         }
         else {
             menu.style.display = "block";
+        }
+        if (status.style.display == "block") {
+            status.style.display = "none";
+        }
+        else {
+            status.style.display = "none";
         }
         if (chat.style.display == "block") {
             chat.style.display = "none";
@@ -57,6 +77,29 @@ var Lightning;
     //     // window.history.back();
     //     window.history.go(-1);
     // }
+    function startStatus(_event) {
+        console.log("funktioniert");
+        let menu = document.querySelector("#menuid");
+        let status = document.querySelector("#categoryid");
+        if (menu.style.display == "block") {
+            menu.style.display = "none";
+        }
+        else {
+            menu.style.display = "none";
+        }
+        if (status.style.display == "none") {
+            status.style.display = "block";
+        }
+        else {
+            status.style.display = "block";
+        }
+    }
+    function setColor(_event) {
+        console.log("Es hat funktioniert jiha");
+        let music = document.querySelector("#musicid");
+        let value = music.getAttribute("value");
+        console.log("value:" + value);
+    }
     function startChat(_event) {
         console.log("funktioniert2!");
         let menu = document.querySelector("#menuid");
--- script.ts
+++ script.ts
@@ -1,8 +1,20 @@
 namespace Lightning {
     window.addEventListener("load", handleLoad);
 
+    let selectedcolor: string = "#ff0000";
+
     function handleLoad(_event: Event): void {
     console.log("Anwendung startet");
+
+    // Farbe der Kategorie
+    let music: HTMLDivElement = <HTMLDivElement>document.querySelector("#musicid");
+    music.addEventListener("click", setColor);
+    let film: HTMLDivElement = <HTMLDivElement>document.querySelector("#filmid");
+    film.addEventListener("click", setColor);
+    let sport: HTMLDivElement = <HTMLDivElement>document.querySelector("#sportid");
+    sport.addEventListener("click", setColor);
+    let fitness: HTMLDivElement = <HTMLDivElement>document.querySelector("#fitnessid");
+    fitness.addEventListener("click", setColor);
 
     // Blitz Bild
     let lightning: HTMLElement = <HTMLElement>document.querySelector("#lightningid");
@@ -14,7 +26,10 @@ namespace Lightning {
 
     // let backarrow: HTMLElement = <HTMLElement>document.querySelector("#arrowid");
     // backarrow.addEventListener("click", NavigateBack);
-
+    
+    // Status
+    let statusfield: HTMLDivElement = <HTMLDivElement>document.querySelector("#statusid");
+    statusfield.addEventListener("click", startStatus);
 
     // Chat
     let chatfield: HTMLDivElement = <HTMLDivElement>document.querySelector("#chatfieldid");
@@ -46,11 +61,18 @@ namespace Lightning {
         console.log("navigieren erfolgreich");
         let menu: HTMLElement = <HTMLElement>document.querySelector("#menuid");
         let chat: HTMLElement = <HTMLElement>document.querySelector("#chatid");
+        let status: HTMLElement = <HTMLElement>document.querySelector("#categoryid");
 
         if (menu.style.display == "none") {
             menu.style.display = "block";
         } else {
             menu.style.display = "block";
+        }
+
+        if (status.style.display == "block") {
+            status.style.display = "none";
+        } else {
+            status.style.display = "none";
         }
 
         if (chat.style.display == "block") {
@@ -67,6 +89,33 @@ namespace Lightning {
     //     // window.history.back();
     //     window.history.go(-1);
     // }
+
+    function startStatus(_event: Event): void {
+        console.log("funktioniert");
+        let menu: HTMLElement = <HTMLElement>document.querySelector("#menuid");
+        let status: HTMLElement = <HTMLElement>document.querySelector("#categoryid");
+    
+        if (menu.style.display == "block") {
+            menu.style.display = "none";
+        } else {
+            menu.style.display = "none";
+        }
+
+        if (status.style.display == "none") {
+            status.style.display = "block";
+        } else {
+            status.style.display = "block";
+        }
+    }
+
+    function setColor(_event: Event): void {
+        console.log("Es hat funktioniert jiha");
+        let music: HTMLDivElement = <HTMLDivElement>document.querySelector("#musicid");
+        let value = music.getAttribute("value");
+        console.log("value:" + value);
+ 
+        
+    }
 
     function startChat(_event: Event): void {
         console.log("funktioniert2!");
